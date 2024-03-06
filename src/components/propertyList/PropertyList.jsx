2150dc519e20f94d87dd968a0d921d202414b155
Mohidul Office <mohidulislam.official@gmail.com>
2024-03-06 17:32:09 +0530

    Home Page Complete List Page Start

---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,38 @@
+import "./propertylist.css"
+
+const PropertyList = () => {
+  return (
+    <div className="pList">
+        <div className="pListItem">
+            <img src="https://q-xx.bstatic.com/xdata/images/xphoto/263x210/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" className="pListImg" />
+            <div className="pListTitle">
+                <h1>Hotel</h1>
+                <h2>233 hotels</h2>
+            </div>
+        </div>
+        <div className="pListItem">
+            <img src="https://q-xx.bstatic.com/xdata/images/xphoto/263x210/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" className="pListImg" />
+            <div className="pListTitle">
+                <h1>Hotel</h1>
+                <h2>233 hotels</h2>
+            </div>
+        </div>
+        <div className="pListItem">
+            <img src="https://q-xx.bstatic.com/xdata/images/xphoto/263x210/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" className="pListImg" />
+            <div className="pListTitle">
+                <h1>Hotel</h1>
+                <h2>233 hotels</h2>
+            </div>
+        </div>
+        <div className="pListItem">
+            <img src="https://q-xx.bstatic.com/xdata/images/xphoto/263x210/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" alt="" className="pListImg" />
+            <div className="pListTitle">
+                <h1>Hotel</h1>
+                <h2>233 hotels</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PropertyList
